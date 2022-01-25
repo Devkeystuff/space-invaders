37c5470452c349fa0031f8213bd8fe5a61706bf9
mark-sam27 <90000329+mark-sam27@users.noreply.github.com>
2022-01-25 10:27:49 +0200

    Update ControllerDatabase.py

--- api/controllers/ControllerDatabase.py
+++ api/controllers/ControllerDatabase.py
@@ -6,15 +6,19 @@ from models.requests.RequestSaveResult import RequestSaveResult
 from models.db.db_results import DbResults
 
 
-class ControllerDatabase:
+class ControllerDatabase: 
+    # statiska metode, kura pienem rezultatu db
     @staticmethod
-    async def save_result(result: RequestSaveResult) -> int:
+    async def save_result(result: RequestSaveResult) -> int:     
         try:
             cols = ["points", "username"]
+            # mainigais
 
             print(result)
 
             with DbCursor() as cur:
+                #cur palidz komunicet ar db
+                # izsauca darbibas ar db
                 cur.execute(
                     """INSERT INTO results ({', '.join(cols)}) VALUES (12, 'test') RETURNING result_id"""
                 )
